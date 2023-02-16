56caf9e51cce6668ba304cb51f669a54ee96b47f
AlexD <XZ_DY_GAMING@outlook.com>
2023-02-16 16:01:00 +0800

    first commit

--- src/main/java/com/alexd/messager/JavaMessagerBackEndApplication.java
+++ src/main/java/com/alexd/messager/JavaMessagerBackEndApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaMessagerBackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaMessagerBackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMessagerBackEndApplication.class, args);
+    }
 
 }
--- src/test/java/com/alexd/messager/JavaMessagerBackEndApplicationTests.java
+++ src/test/java/com/alexd/messager/JavaMessagerBackEndApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JavaMessagerBackEndApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
