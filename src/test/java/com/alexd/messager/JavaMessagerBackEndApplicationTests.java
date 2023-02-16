c3dbaa1656a8ac360ea81a659492cd6120128e13
AlexD <xz_dy_gaming@outlook.com>
2023-02-16 15:40:20 +0800

    first commit

---
+++ src/test/java/com/alexd/messager/JavaMessagerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexd.messager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessagerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
