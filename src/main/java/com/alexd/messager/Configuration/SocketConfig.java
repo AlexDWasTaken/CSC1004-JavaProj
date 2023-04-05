c17f3a5b1a8e8636c430b68e4818fd014da0633e
AlexD <xz_dy_gaming@outlook.com>
2023-04-06 00:03:24 +0800

    Finished CORS Problem

---
+++ src/main/java/com/alexd/messager/Configuration/SocketConfig.java
@@ -0,0 +1,27 @@
+package com.alexd.messager.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    @CrossOrigin
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    @CrossOrigin
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socketMessaging").setAllowedOriginPatterns("*").withSockJS();
+    }
+
+}
---
+++ src/main/java/com/alexd/messager/Controller/SocketController.java
@@ -0,0 +1,20 @@
+package com.alexd.messager.Controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SocketController {
+
+//    @MessageMapping("/message")
+//    @SendTo("/topic/newMessage")
+//    public String sendMessage(String message) {
+//        return message;
+//    }
+
+}
